bfaecd4525bbbc552caf74224f2c3dd53491bee9
AlexandruRicioiu <94838609+AlexandruRicioiu@users.noreply.github.com>
2024-03-16 16:14:40 +0200

    first implementation for animal sound project

---
+++ Animal Project/Animal Project/AnimalS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_Project
+{
+    internal class AnimalS
+    {
+        string[] animals = { "pig", "hen", "dog", "cat"};
+        string[] sounds = {"groh", "piu", "ham", "miua" };
+
+        public void AnimalSound()
+        {
+            for (int i = 0; i < animals.Length; i++)
+            {
+                Console.WriteLine( animals[i] + " makes sound " + sounds[i] + " " + sounds[i]);
+            }
+        }
+    }
+}
---
+++ Animal Project/Animal Project/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AnimalS animale = new AnimalS();
+
+            Console.WriteLine("Sounds of animals : \n");
+            animale.AnimalSound();
+
+            Console.Read();
+        }
+    }
+}
